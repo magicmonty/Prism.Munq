160adc457e05fb6dbf9998b70143231510b15370
Martin Gondermann <magicmonty@pagansoft.de>
2016-11-06 22:45:59 +0100

    Fix Build

--- src/Prism.Munq.Wpf.Tests/MunqBootstrapperFixture.cs
+++ src/Prism.Munq.Wpf.Tests/MunqBootstrapperFixture.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Reflection;
 using System.Windows;
+using System.Windows.Controls;
 using Prism.IocContainer.Wpf.Tests.Support;
 using Prism.IocContainer.Wpf.Tests.Support.Mocks;
 using Prism.Logging;
@@ -13,112 +14,112 @@ namespace Prism.Munq.Wpf.Tests
     public class MunqBootstrapperFixture : BootstrapperFixtureBase
     {
         [Fact]
-        public void ContainerDefaultsToNull()
+        public void ContainerDefaultsToNull ()
         {
-            var bootstrapper = new DefaultMunqBootstrapper();
+            var bootstrapper = new DefaultMunqBootstrapper ();
             var container = bootstrapper.Container;
 
-            Assert.Null(container);
+            Assert.Null (container);
         }
 
         [Fact]
-        public void CanCreateConcreteBootstrapper()
+        public void CanCreateConcreteBootstrapper ()
         {
-            new DefaultMunqBootstrapper();
+            new DefaultMunqBootstrapper ();
         }
 
         [Fact]
-        public void CreateContainerShouldInitializeContainer()
+        public void CreateContainerShouldInitializeContainer ()
         {
-            var bootstrapper = new DefaultMunqBootstrapper();
+            var bootstrapper = new DefaultMunqBootstrapper ();
 
-            IIocContainer container = bootstrapper.CallCreateContainer();
+            IIocContainer container = bootstrapper.CallCreateContainer ();
 
-            Assert.NotNull(container);
-            Assert.IsType(typeof(MunqContainerWrapper), container);
+            Assert.NotNull (container);
+            Assert.IsType (typeof (MunqContainerWrapper), container);
         }
 
         [Fact]
-        public void ConfigureContainerAddsModuleCatalogToContainer()
+        public void ConfigureContainerAddsModuleCatalogToContainer ()
         {
-            var bootstrapper = new DefaultMunqBootstrapper();
-            bootstrapper.Run();
+            var bootstrapper = new DefaultMunqBootstrapper ();
+            bootstrapper.Run ();
 
-            var returnedCatalog = bootstrapper.BaseContainer.Resolve<IModuleCatalog>();
-            Assert.NotNull(returnedCatalog);
-            Assert.True(returnedCatalog is ModuleCatalog);
+            var returnedCatalog = bootstrapper.BaseContainer.Resolve<IModuleCatalog> ();
+            Assert.NotNull (returnedCatalog);
+            Assert.True (returnedCatalog is ModuleCatalog);
         }
 
         [Fact]
-        public void ConfigureContainerAddsLoggerFacadeToContainer()
+        public void ConfigureContainerAddsLoggerFacadeToContainer ()
         {
-            var bootstrapper = new DefaultMunqBootstrapper();
-            bootstrapper.Run();
+            var bootstrapper = new DefaultMunqBootstrapper ();
+            bootstrapper.Run ();
 
-            var returnedCatalog = bootstrapper.BaseContainer.Resolve<ILoggerFacade>();
-            Assert.NotNull(returnedCatalog);
+            var returnedCatalog = bootstrapper.BaseContainer.Resolve<ILoggerFacade> ();
+            Assert.NotNull (returnedCatalog);
         }
 
         [Fact]
-        public void ConfigureContainerAddsRegionNavigationJournalEntryToContainer()
+        public void ConfigureContainerAddsRegionNavigationJournalEntryToContainer ()
         {
-            var bootstrapper = new DefaultMunqBootstrapper();
-            bootstrapper.Run();
+            var bootstrapper = new DefaultMunqBootstrapper ();
+            bootstrapper.Run ();
 
-            var actual1 = bootstrapper.BaseContainer.Resolve<IRegionNavigationJournalEntry>();
-            var actual2 = bootstrapper.BaseContainer.Resolve<IRegionNavigationJournalEntry>();
+            var actual1 = bootstrapper.BaseContainer.Resolve<IRegionNavigationJournalEntry> ();
+            var actual2 = bootstrapper.BaseContainer.Resolve<IRegionNavigationJournalEntry> ();
 
-            Assert.NotNull(actual1);
-            Assert.NotNull(actual2);
-            Assert.NotSame(actual1, actual2);
+            Assert.NotNull (actual1);
+            Assert.NotNull (actual2);
+            Assert.NotSame (actual1, actual2);
         }
 
         [Fact]
-        public void ConfigureContainerAddsRegionNavigationJournalToContainer()
+        public void ConfigureContainerAddsRegionNavigationJournalToContainer ()
         {
-            var bootstrapper = new DefaultMunqBootstrapper();
-            bootstrapper.Run();
+            var bootstrapper = new DefaultMunqBootstrapper ();
+            bootstrapper.Run ();
 
-            var actual1 = bootstrapper.BaseContainer.Resolve<IRegionNavigationJournal>();
-            var actual2 = bootstrapper.BaseContainer.Resolve<IRegionNavigationJournal>();
+            var actual1 = bootstrapper.BaseContainer.Resolve<IRegionNavigationJournal> ();
+            var actual2 = bootstrapper.BaseContainer.Resolve<IRegionNavigationJournal> ();
 
-            Assert.NotNull(actual1);
-            Assert.NotNull(actual2);
-            Assert.NotSame(actual1, actual2);
+            Assert.NotNull (actual1);
+            Assert.NotNull (actual2);
+            Assert.NotSame (actual1, actual2);
         }
 
         [Fact]
-        public void ConfigureContainerAddsRegionNavigationServiceToContainer()
+        public void ConfigureContainerAddsRegionNavigationServiceToContainer ()
         {
-            var bootstrapper = new DefaultMunqBootstrapper();
-            bootstrapper.Run();
+            var bootstrapper = new DefaultMunqBootstrapper ();
+            bootstrapper.Run ();
 
-            var actual1 = bootstrapper.BaseContainer.Resolve<IRegionNavigationService>();
-            var actual2 = bootstrapper.BaseContainer.Resolve<IRegionNavigationService>();
+            var actual1 = bootstrapper.BaseContainer.Resolve<IRegionNavigationService> ();
+            var actual2 = bootstrapper.BaseContainer.Resolve<IRegionNavigationService> ();
 
-            Assert.NotNull(actual1);
-            Assert.NotNull(actual2);
-            Assert.NotSame(actual1, actual2);
+            Assert.NotNull (actual1);
+            Assert.NotNull (actual2);
+            Assert.NotSame (actual1, actual2);
         }
 
         [Fact]
-        public void ConfigureContainerAddsNavigationTargetHandlerToContainer()
+        public void ConfigureContainerAddsNavigationTargetHandlerToContainer ()
         {
-            var bootstrapper = new DefaultMunqBootstrapper();
-            bootstrapper.Run();
+            var bootstrapper = new DefaultMunqBootstrapper ();
+            bootstrapper.Run ();
 
-            var actual1 = bootstrapper.BaseContainer.Resolve<IRegionNavigationContentLoader>();
-            var actual2 = bootstrapper.BaseContainer.Resolve<IRegionNavigationContentLoader>();
+            var actual1 = bootstrapper.BaseContainer.Resolve<IRegionNavigationContentLoader> ();
+            var actual2 = bootstrapper.BaseContainer.Resolve<IRegionNavigationContentLoader> ();
 
-            Assert.NotNull(actual1);
-            Assert.NotNull(actual2);
-            Assert.Same(actual1, actual2);
+            Assert.NotNull (actual1);
+            Assert.NotNull (actual2);
+            Assert.Same (actual1, actual2);
         }
     }
 
     internal class DefaultMunqBootstrapper : MunqBootstrapper
     {
-        public List<string> MethodCalls = new List<string>();
+        public List<string> MethodCalls = new List<string> ();
         public bool InitializeModulesCalled;
         public bool ConfigureRegionAdapterMappingsCalled;
         public RegionAdapterMappings DefaultRegionAdapterMappings;
@@ -132,120 +133,119 @@ namespace Prism.Munq.Wpf.Tests
         public bool ConfigureServiceLocatorCalled;
         public bool ConfigureViewModelLocatorCalled;
         public bool ConfigureDefaultRegionBehaviorsCalled;
-        public DependencyObject ShellObject = new UserControl();
+        public DependencyObject ShellObject = new UserControl ();
 
         public DependencyObject BaseShell => Shell;
 
-        public IIocContainer BaseContainer
-        {
+        public IIocContainer BaseContainer {
             get { return Container; }
             set { Container = value; }
         }
 
         public MockLoggerAdapter BaseLogger => Logger as MockLoggerAdapter;
 
-        public IIocContainer CallCreateContainer()
+        public IIocContainer CallCreateContainer ()
         {
-            return CreateContainer();
+            return CreateContainer ();
         }
 
-        protected override IIocContainer CreateContainer()
+        protected override IIocContainer CreateContainer ()
         {
-            MethodCalls.Add(MethodBase.GetCurrentMethod().Name);
+            MethodCalls.Add (MethodBase.GetCurrentMethod ().Name);
             CreateContainerCalled = true;
-            return base.CreateContainer();
+            return base.CreateContainer ();
         }
 
-        protected override void ConfigureContainer()
+        protected override void ConfigureContainer ()
         {
-            MethodCalls.Add(MethodBase.GetCurrentMethod().Name);
+            MethodCalls.Add (MethodBase.GetCurrentMethod ().Name);
             ConfigureContainerCalled = true;
-            base.ConfigureContainer();
+            base.ConfigureContainer ();
         }
 
-        protected override ILoggerFacade CreateLogger()
+        protected override ILoggerFacade CreateLogger ()
         {
-            MethodCalls.Add(MethodBase.GetCurrentMethod().Name);
+            MethodCalls.Add (MethodBase.GetCurrentMethod ().Name);
             CreateLoggerCalled = true;
-            return new MockLoggerAdapter();
+            return new MockLoggerAdapter ();
         }
 
-        protected override DependencyObject CreateShell()
+        protected override DependencyObject CreateShell ()
         {
-            MethodCalls.Add(MethodBase.GetCurrentMethod().Name);
+            MethodCalls.Add (MethodBase.GetCurrentMethod ().Name);
             CreateShellCalled = true;
             return ShellObject;
         }
 
-        protected override void ConfigureServiceLocator()
+        protected override void ConfigureServiceLocator ()
         {
-            MethodCalls.Add(MethodBase.GetCurrentMethod().Name);
+            MethodCalls.Add (MethodBase.GetCurrentMethod ().Name);
             ConfigureServiceLocatorCalled = true;
-            base.ConfigureServiceLocator();
+            base.ConfigureServiceLocator ();
         }
 
-        protected override void ConfigureViewModelLocator()
+        protected override void ConfigureViewModelLocator ()
         {
-            MethodCalls.Add(MethodBase.GetCurrentMethod().Name);
+            MethodCalls.Add (MethodBase.GetCurrentMethod ().Name);
             ConfigureViewModelLocatorCalled = true;
-            base.ConfigureViewModelLocator();
+            base.ConfigureViewModelLocator ();
         }
 
-        protected override IModuleCatalog CreateModuleCatalog()
+        protected override IModuleCatalog CreateModuleCatalog ()
         {
-            MethodCalls.Add(MethodBase.GetCurrentMethod().Name);
+            MethodCalls.Add (MethodBase.GetCurrentMethod ().Name);
             CreateModuleCatalogCalled = true;
-            return base.CreateModuleCatalog();
+            return base.CreateModuleCatalog ();
         }
 
-        protected override void ConfigureModuleCatalog()
+        protected override void ConfigureModuleCatalog ()
         {
-            MethodCalls.Add(MethodBase.GetCurrentMethod().Name);
+            MethodCalls.Add (MethodBase.GetCurrentMethod ().Name);
             ConfigureModuleCatalogCalled = true;
-            base.ConfigureModuleCatalog();
+            base.ConfigureModuleCatalog ();
         }
 
-        protected override void InitializeShell()
+        protected override void InitializeShell ()
         {
-            MethodCalls.Add(MethodBase.GetCurrentMethod().Name);
+            MethodCalls.Add (MethodBase.GetCurrentMethod ().Name);
             InitializeShellCalled = true;
             // no op
         }
 
-        protected override void InitializeModules()
+        protected override void InitializeModules ()
         {
-            MethodCalls.Add(MethodBase.GetCurrentMethod().Name);
+            MethodCalls.Add (MethodBase.GetCurrentMethod ().Name);
             InitializeModulesCalled = true;
-            base.InitializeModules();
+            base.InitializeModules ();
         }
 
-        protected override IRegionBehaviorFactory ConfigureDefaultRegionBehaviors()
+        protected override IRegionBehaviorFactory ConfigureDefaultRegionBehaviors ()
         {
-            MethodCalls.Add(MethodBase.GetCurrentMethod().Name);
+            MethodCalls.Add (MethodBase.GetCurrentMethod ().Name);
             ConfigureDefaultRegionBehaviorsCalled = true;
-            return base.ConfigureDefaultRegionBehaviors();
+            return base.ConfigureDefaultRegionBehaviors ();
         }
 
-        protected override RegionAdapterMappings ConfigureRegionAdapterMappings()
+        protected override RegionAdapterMappings ConfigureRegionAdapterMappings ()
         {
-            MethodCalls.Add(MethodBase.GetCurrentMethod().Name);
+            MethodCalls.Add (MethodBase.GetCurrentMethod ().Name);
             ConfigureRegionAdapterMappingsCalled = true;
-            var regionAdapterMappings = base.ConfigureRegionAdapterMappings();
+            var regionAdapterMappings = base.ConfigureRegionAdapterMappings ();
 
             DefaultRegionAdapterMappings = regionAdapterMappings;
 
             return regionAdapterMappings;
         }
 
-        protected override void RegisterFrameworkExceptionTypes()
+        protected override void RegisterFrameworkExceptionTypes ()
         {
-            MethodCalls.Add(MethodBase.GetCurrentMethod().Name);
-            base.RegisterFrameworkExceptionTypes();
+            MethodCalls.Add (MethodBase.GetCurrentMethod ().Name);
+            base.RegisterFrameworkExceptionTypes ();
         }
 
-        public void CallRegisterFrameworkExceptionTypes()
+        public void CallRegisterFrameworkExceptionTypes ()
         {
-            base.RegisterFrameworkExceptionTypes();
+            base.RegisterFrameworkExceptionTypes ();
         }
     }
 

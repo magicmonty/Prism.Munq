8dbb04ea3500a62a7b606443f09a4bbce1c4c944
Martin Gondermann <magicmonty@pagansoft.de>
2016-11-06 23:13:42 +0100

    Removed CLSCompliant attribute

--- src/Prism.IocContainer.Wpf.Tests.Support/Properties/AssemblyInfo.cs
+++ src/Prism.IocContainer.Wpf.Tests.Support/Properties/AssemblyInfo.cs
@@ -5,25 +5,26 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+using System;
 
-[assembly: AssemblyTitle("Prism.IocContainer.Wpf.Tests.Support")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Prism.IocContainer.Wpf.Tests.Support")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle ("Prism.IocContainer.Wpf.Tests.Support")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("Prism.IocContainer.Wpf.Tests.Support")]
+[assembly: AssemblyCopyright ("Copyright ©  2016")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 
-[assembly: ComVisible(false)]
+[assembly: ComVisible (false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
-[assembly: Guid("C98AC131-04A0-4847-AF4F-8B6E58C615D3")]
+[assembly: Guid ("C98AC131-04A0-4847-AF4F-8B6E58C615D3")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -36,5 +37,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion ("1.0.0.0")]
+[assembly: AssemblyFileVersion ("1.0.0.0")]
+[assembly: CLSCompliant (false)]
--- src/Prism.Munq.Wpf/Properties/AssemblyInfo.cs
+++ src/Prism.Munq.Wpf/Properties/AssemblyInfo.cs
@@ -5,21 +5,21 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Prism.Munq.Wpf")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Prism")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: CLSCompliant(true)]
+[assembly: AssemblyTitle ("Prism.Munq.Wpf")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("Prism")]
+[assembly: AssemblyCopyright ("Copyright ©  2015")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+[assembly: CLSCompliant (false)]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible (false)]
 
-[assembly: AssemblyVersion("6.2")]
-[assembly: AssemblyFileVersion("6.2.0")]
-[assembly: AssemblyInformationalVersion("6.2.0")]
+[assembly: AssemblyVersion ("6.2")]
+[assembly: AssemblyFileVersion ("6.2.0")]
+[assembly: AssemblyInformationalVersion ("6.2.0")]

e328bb3755b95a4ce1cb6a409c14f0fe5536969f
Martin Gondermann <magicmonty@pagansoft.de>
2016-11-06 22:03:13 +0100

    First try to fix failing build

--- src/Prism.IocContainer.Wpf.Tests.Support/BootstrapperFixtureBase.cs
+++ src/Prism.IocContainer.Wpf.Tests.Support/BootstrapperFixtureBase.cs
@@ -5,23 +5,19 @@ namespace Prism.IocContainer.Wpf.Tests.Support
 {
     public class BootstrapperFixtureBase
     {
-        protected static void AssertExceptionThrownOnRun(Bootstrapper bootstrapper, Type expectedExceptionType, string expectedExceptionMessageSubstring)
+        protected static void AssertExceptionThrownOnRun (Bootstrapper bootstrapper, Type expectedExceptionType, string expectedExceptionMessageSubstring)
         {
             bool exceptionThrown = false;
-            try
-            {
-                bootstrapper.Run();
-            }
-            catch (Exception ex)
-            {
-                Assert.Equal(expectedExceptionType, ex.GetType());
-                Assert.Contains(expectedExceptionMessageSubstring, ex.Message);
+            try {
+                bootstrapper.Run ();
+            } catch (Exception ex) {
+                Assert.Equal (expectedExceptionType, ex.GetType ());
+                Assert.Contains (expectedExceptionMessageSubstring, ex.Message);
                 exceptionThrown = true;
             }
 
-            if (!exceptionThrown)
-            {
-                Assert.Fail("Exception not thrown.");
+            if (!exceptionThrown) {
+                throw new Exception ("Exception not thrown.");
             }
         }
     }

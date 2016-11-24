2cb63ae79fe4394665fdafcea16687c08a63fdbb
Martin Gondermann <mgondermann@np4.net>
2016-11-24 13:03:57 +0100

    Added AppVeyor support

--- build.fsx
+++ build.fsx
@@ -2,21 +2,55 @@
 
 open Fake
 open Fake.Testing.NUnit3
+open ReleaseNotesHelper
+open BuildServerHelper
+open AssemblyInfoHelper
 
-let outputDirectory = "out"
+let outputDirectory = @"out"
+let releaseNotes = LoadReleaseNotes "ReleaseNotes.md"
+
+let version = 
+  match buildServer with
+  | AppVeyor -> { releaseNotes.SemVer with Build = appVeyorBuildVersion }
+  | _ -> releaseNotes.SemVer
+let buildNumber = match version.Build with | "" -> "" | v -> "." + v
+let asmVersion = sprintf "%i.%i.%i%s" version.Major version.Minor version.Patch (match version.Build with | "" -> "" | v -> "." + v)
+let nugetVersion = sprintf "%i.%i.%i%s" version.Major version.Minor version.Patch (match version.Build with | "" -> "" | v -> "-build" + v)
+
+tracefn "Assembly Version: %s" asmVersion
+tracefn "NuGet Version: %s" nugetVersion
 
 Target "Clean" (fun _ ->
-  outputDirectory |> DeleteDir
-
   !! "**/bin"
   ++ "**/obj"
+  ++ "**/out"
   |> DeleteDirs
 )
 
+Target "PatchAssemblyInfos" (fun _ ->
+  let assemblyInfos = !!"src/**/AssemblyInfo.cs"
+  ReplaceAssemblyInfoVersionsBulk assemblyInfos (fun f -> { f with AssemblyVersion = asmVersion
+                                                                   AssemblyInformationalVersion = asmVersion
+                                                                   AssemblyFileVersion = asmVersion }) 
+)
+
 Target "Build" (fun _ ->
-  !! "src/Prism.Munq.sln"
-  |> MSBuildDebug outputDirectory "Clean,Build" 
-  |> Log "Build-Output: "
+  let setParams defaults = 
+    { defaults with
+        Verbosity = Some(Quiet)
+        NodeReuse = false
+        NoLogo = true
+        Targets = ["Build"]
+        Properties = 
+          [
+            "Optimize", "True"
+            "DebugSymbols", "True"
+            "Configuration", "Debug"
+            "OutputPath", outputDirectory |> FullName |> trimSeparator
+          ]
+    }
+  
+  build setParams "./src/Prism.Munq.sln" |> ignore
 )
 
 Target "Test" (fun _ ->
@@ -29,7 +63,8 @@ Target "Test" (fun _ ->
 )
 
 "Clean"
+  =?> ("PatchAssemblyInfos", not isLocalBuild)
   ==> "Build"
-  ==> "Test"
+  =?> ("Test", outputDirectory |> directoryExists |> not)
 
 RunTargetOrDefault "Build"
